ab542a716e230bb928eaccc1f303cd9a275dd3b5
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-06 06:32:40 +0545

    props

--- src/Test.jsx
+++ src/Test.jsx
@@ -11,18 +11,6 @@ function Test(){
                 subtitle="Grab the Chance to Win a Khasi in Our Dashain Offer!" 
                 description="We're offering a special Dashain deal on our courses at an unbeatable price of just Rs. 35K! Plus, you'll have a chance to win an exciting prize—a big Khasi weighing around 5 KG!"
                 bgColor="" />
-                <Card 
-                image="https://static.vecteezy.com/system/resources/thumbnails/023/055/999/small_2x/funny-goat-smile-generative-ai-photo.jpg"
-                title="Dashain Offer" 
-                subtitle="Grab the Chance to Win a Khasi in Our Dashain Offer!" 
-                description="We're offering a special Dashain deal on our courses at an unbeatable price of just Rs. 35K! Plus, you'll have a chance to win an exciting prize—a big Khasi weighing around 5 KG!"
-                bgColor="" />
-                <Card 
-                image="https://static.vecteezy.com/system/resources/thumbnails/023/055/999/small_2x/funny-goat-smile-generative-ai-photo.jpg"
-                title="Dashain Offer" 
-                subtitle="Grab the Chance to Win a Khasi in Our Dashain Offer!" 
-                description="We're offering a special Dashain deal on our courses at an unbeatable price of just Rs. 35K! Plus, you'll have a chance to win an exciting prize—a big Khasi weighing around 5 KG!"
-                bgColor="" />
             </div>
         </>
     )
